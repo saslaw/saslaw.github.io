ab9062cc1c36921d5670873f1c31d6fc0fa83299
mae <saslaw@pdx.edu>
2021-02-21 12:34:15 -0500

    added a very small app
    deployed to shinyapps.io

--- apps/GNIP-daily-0.1/app.R
+++ apps/GNIP-daily-0.1/app.R
@@ -1,5 +1,10 @@
 
+# This is the RStudio sample code, with the GNIP data instead of "Old Faithful" sample data. 
+
+# Libraries
 library(shiny)
+library(rsconnect)
+library(tidyverse)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -27,19 +32,21 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
+    
     output$distPlot <- renderPlot({
         # generate bins based on input$bins from ui.R
         x    <- waters.app$O18 
         bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
         # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        hist(x, breaks = bins, col = '#66CCEE', border = 'white',
+             xlab = expression(paste(delta^{18}, "O (‰ VSMOW)")),
+             main = NULL)
     })
 }
 
 # Load and clean data 
-waters.all <- read.csv(here("data/water/GNIP-daily_2010-2020.csv"))
+waters.all <- read.csv("GNIP-daily_2010-2020.csv")
 waters.app <- waters.all %>% drop_na(O18)
 
 # Run the application 
